89a623b5988e7b3d580377e330839ef506d0c3be
JIGYUN <wlrbs1492@gmail.com>
2025-10-15 10:39:50 +0900

    ddd

--- postcss.config.mjs
+++
@@ -1,7 +0,0 @@
-// postcss.config.mjs  (Tailwind v4 권장 형태)
-export default {
-  plugins: {
-    '@tailwindcss/postcss': {},  // ← v4에서 필요한 공식 PostCSS 플러그인
-    autoprefixer: {},            // 선택이지만 함께 두어도 OK
-  },
-};
